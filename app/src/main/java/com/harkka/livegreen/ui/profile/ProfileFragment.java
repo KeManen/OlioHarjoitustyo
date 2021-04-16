f5a173093aadf2543f7452f86f32cf73edaa9aea
KeMaAid <konsta.keski-mattinen@wippies.fi>
2021-04-16 18:22:26 +0300

    profilescreen loginbutton 50%

--- app/src/main/java/com/harkka/livegreen/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/harkka/livegreen/ui/profile/ProfileFragment.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -17,6 +19,8 @@ import com.harkka.livegreen.R;
 public class ProfileFragment extends Fragment {
 
     private ProfileViewModel profileViewModel;
+    private Button login_button;
+    private CardView card;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,6 +28,14 @@ public class ProfileFragment extends Fragment {
                 new ViewModelProvider(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
         final TextView textView = root.findViewById(R.id.text_profile);
+        login_button = root.findViewById(R.id.buttonProfileViewLogout);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handle_profileview_loginbutton();
+            }
+        });
+        card = root.findViewById(R.id.profileCardView);
         profileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
@@ -31,5 +43,33 @@ public class ProfileFragment extends Fragment {
             }
         });
         return root;
+    }
+
+    public void handle_profileview_loginbutton(){
+        // TODO toggle current login position
+        handle_profileview_login_state(false);
+    }
+
+    // Change profileview according to loginstate
+    public void handle_profileview_login_state(boolean is_logged){
+        //login_button = v.findViewById(R.id.buttonProfileViewLogout);
+        //card = v.findViewById(R.id.profileCardView);
+
+        if(is_logged){
+            login_button.setText(getText(R.string.log_out));
+            login_button.setBackgroundColor(login_button.getContext().getResources().getColor(R.color.red));
+
+            card.setVisibility(View.VISIBLE);
+
+            //TODO change rank, profile picture and profile name with stockvalues
+
+        } else {
+            login_button.setText(getText(R.string.log_in));
+            login_button.setBackgroundColor(login_button.getContext().getResources().getColor(R.color.green_dark));
+
+            card.setVisibility(View.GONE);
+
+            //TODO change rank, profile picture and profile name with stockvalues
+        }
     }
 }
