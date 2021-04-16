abdebef124bccca463656db0a377c9ea83140472
ArttuJahva <77405747+ArttuJahva@users.noreply.github.com>
2021-04-16 22:55:15 +0300

    Update BarChartActivity.java

--- BarChart/BarChartActivity.java
+++ BarChart/BarChartActivity.java
@@ -1,9 +1,9 @@
 package com.example.barchart;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.Color;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
@@ -12,6 +12,8 @@ import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+
 
 public class BarChartActivity extends AppCompatActivity {
 
@@ -23,11 +25,15 @@ public class BarChartActivity extends AppCompatActivity {
         // initialize components
         BarChart barChart = findViewById(R.id.barChart);
         ArrayList<BarEntry> emissions = new ArrayList<>();
-      
-      //TODO add data inserts from database
-      //TODO max one month per one chart
-      //TODO when month is full --> move data to file?
-      //TODO get date as number
+
+        //TODO add data inserts from database
+        //TODO max one month per one chart
+        //TODO when month is full --> move data to file?
+
+        // get date as number of the month
+        Calendar c = Calendar.getInstance();
+        int wantedDate = c.get(Calendar.DAY_OF_MONTH);
+        System.out.println(wantedDate);
 
         // test emissions for a week
         // format is day of the month (int) + emissions
@@ -45,13 +51,14 @@ public class BarChartActivity extends AppCompatActivity {
         emissions.add(new BarEntry(14, 620));
         emissions.add(new BarEntry(20, 0));
         emissions.add(new BarEntry(21, 700));
+        emissions.add(new BarEntry(28, 400));
         emissions.add(new BarEntry(29, 320));
         emissions.add(new BarEntry(30, 20)); */
 
         // create dataset using library and specify text size and colors
         BarDataSet barDataSet = new BarDataSet(emissions, "Emissions");
         barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
-        barDataSet.setValueTextColor(Color.DKGRAY);
+        barDataSet.setValueTextColor(Color.BLACK);
         barDataSet.setValueTextSize(16f);
 
         // fill the bardata with arraylist data
